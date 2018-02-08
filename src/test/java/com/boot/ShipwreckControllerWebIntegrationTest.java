54e1b85de3b937252cb4f698aadcc8aaf4b1621d
Michael Morris <michael.morris@ruralsource.com>
2018-02-08 12:15:02 -0600

    maven surefire config change required

--- src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
+++ src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
@@ -15,7 +15,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.databind.JsonNode;
