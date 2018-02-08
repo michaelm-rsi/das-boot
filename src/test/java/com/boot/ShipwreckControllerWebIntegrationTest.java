767b6ef6b9ccf6bce62add33a060fa55e0fb81de
Michael Morris <michael.morris@ruralsource.com>
2018-02-08 11:50:13 -0600

    Externalize host/port settings for CI

--- src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
+++ src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
@@ -8,12 +8,15 @@ import java.io.IOException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,11 +24,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
 public class ShipwreckControllerWebIntegrationTest {
+	@Value("${server.address}")
+	private String serverHost;
+	
+	@Value("${server.port}")
+	private String serverPort;
+	
 	@Test
 	public void testListAll() throws IOException {
 		TestRestTemplate restTemplate = new TestRestTemplate();
 		
-		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api/v1/shipwrecks", String.class);
+		UriComponentsBuilder url = UriComponentsBuilder.newInstance()
+			.scheme("http")
+			.host(serverHost)
+			.port(serverPort)
+			.path("/api/v1/shipwrecks");
+		
+		ResponseEntity<String> response = restTemplate.getForEntity(url.toUriString(), String.class);
 		
 		assertThat( response.getStatusCode(), equalTo(HttpStatus.OK));
 		
@@ -37,45 +52,3 @@ public class ShipwreckControllerWebIntegrationTest {
 	}
 }
 
-/*
-package com.boot;
-
-import java.io.IOException;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(App.class)
-@WebIntegrationTest
-public class ShipwreckControllerWebIntegrationTest {
-	@Test
-	public void testListAll() throws IOException {
-		RestTemplate restTemplate = new TestRestTemplate();
-		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api/v1/shipwrecks", String.class);
-
-	    assertThat( response.getStatusCode() , equalTo(HttpStatus.OK));
-
-	    ObjectMapper objectMapper = new ObjectMapper();
-	    JsonNode responseJson = objectMapper.readTree(response.getBody());
-
-	    assertThat( responseJson.isMissingNode() , is(false) );
-	    assertThat( responseJson.toString() , equalTo("[]") );
-		
-	}
-
-}
-*/
