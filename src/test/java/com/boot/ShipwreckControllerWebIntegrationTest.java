98116f41ceea2e5212a99dc5f53f0e44305286b7
Michael Morris <michael.morris@ruralsource.com>
2018-02-08 10:36:36 -0600

    Lets see if this test fail was a local firewall issue

--- src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
+++ src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
@@ -23,15 +23,14 @@ import java.io.IOException;
 public class ShipwreckControllerWebIntegrationTest {
 	@Test
 	public void testListAll() throws IOException {
-		return;
-//		TestRestTemplate restTemplate = new TestRestTemplate();
-//		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api/v1/shipwrecks", String.class);
-//		
-//		assertThat( response.getStatusCode(), equalTo(HttpStatus.SC_OK));
-//		ObjectMapper objectMapper = new ObjectMapper();
-//		JsonNode responseJson = objectMapper.readTree(response.getBody());
-//		
-//		assertThat( responseJson.isMissingNode(), is(false));
-//		assertThat( responseJson.toString(), equalTo("[]"));
+		TestRestTemplate restTemplate = new TestRestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api/v1/shipwrecks", String.class);
+		
+		assertThat( response.getStatusCode(), equalTo(HttpStatus.SC_OK));
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode responseJson = objectMapper.readTree(response.getBody());
+		
+		assertThat( responseJson.isMissingNode(), is(false));
+		assertThat( responseJson.toString(), equalTo("[]"));
 	}
 }
